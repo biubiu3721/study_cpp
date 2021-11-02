068580dc9071e227bd8b07593943e906d3f23fe7
focusunsink <549303468@qq.com>
2021-11-02 03:59:57 -0700

    Test ssh

---
+++ experiments/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main()
+{
+    int sum = 0;
+    for (int i = 0; i < 100; i++)
+    {
+	    sum++;
+
+    }
+    printf("It is over!\n");
+    return 0;
+
+}
