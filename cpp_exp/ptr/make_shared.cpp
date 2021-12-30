660440451e44243c0b3b66d1d0280deca7e2b1b9
focusunsink <549303468@qq.com>
2021-12-29 19:36:18 -0800

    [memory][make_shared] finish practice

---
+++ cpp_exp/ptr/make_shared.cpp
@@ -0,0 +1,17 @@
+#include "../../include/log.h"
+#include <memory>
+#include <vector>
+int main()
+{
+    std::shared_ptr<int> foo = std::make_shared<int> (10);
+    std::shared_ptr<int> foo2(new int(100)); // 
+    auto bar = std::make_shared<int>(20); // const -> obj -> ptr
+    auto baz = std::make_shared<std::pair<int, int>> (30, 40); // const -> obj -> ptr
+    DEBUG() << foo << " " << *foo2 <<  ENDL;
+    DEBUG() << foo2 << " " << *foo2 <<  ENDL;
+    DEBUG() << bar << " " << *bar << ENDL;
+    DEBUG() << baz << " " << baz->first << " " << baz->second << ENDL;
+
+
+    ;
+}
--- cpp_exp/virtual/main.cpp
+++ cpp_exp/virtual/main.cpp
@@ -3,29 +3,33 @@
 class grand
 {
   public:
-  virtual void Info() = 0;
-    /*{
+  virtual void Info()
+  {
         DEBUG() << "I am a grand" << ENDL;
-    }*/
+  }
 };
 
 class father : public grand
 {
   public:
-  virtual void Info() = 0;
-  /*
+  void Info()
+  
   {
         DEBUG() << "I am a father" << ENDL;
-  }*/
+  }
 };
 
 
 class son : public father
 {
   public:
-  virtual void Info()
+  void Info()
   {
         DEBUG() << "I am a son" << ENDL;
+  }
+  void son_only()
+  {
+       DEBUG() << "only son" << ENDL;
   }
 };
 
@@ -33,12 +37,13 @@ class son : public father
 int main()
 {
     grand * grand_ptr = new son;
-    father * father_ptr = new son;
-    son * son_ptr = new son;
+    father * father_ptr = new son; 
+    son * son_ptr = new son;  
+
     DEBUG() << "grand ptr:  "; 
-    grand_ptr->Info();
+    grand_ptr->Info(); 
     DEBUG() << "father ptr:  ";
-    father_ptr->Info();
+    father_ptr->Info(); // Info(*father)
     DEBUG() << "son ptr:  ";
-    son_ptr->Info();
+    son_ptr->Info(); // Info(*son)
 }
